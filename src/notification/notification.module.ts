cc441b4d54e90a5abb518d26d5952ac8935a432b
ernestogalarza87 <ernestoagc87@gmail.com>
2025-01-08 02:23:07 +0100

    removing firebase old setting

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './services/notification.service';
 import { NotificationController } from './controller/notification.controller';
-import { firebaseAdminProvider } from './services/firebase-admin.provider';
 import { MongooseModule, Schema } from '@nestjs/mongoose';
 import { AlertNotification, AlertNotificationSchema } from './entity/alert-notification.entity';
 import { HttpModule } from '@nestjs/axios';
 import { HttpHelperService } from '../common/http/http-helper.service';
 
 @Module({
-  providers: [NotificationService,firebaseAdminProvider,HttpHelperService],
+  providers: [NotificationService,HttpHelperService],
   controllers: [NotificationController],
   exports:[NotificationService],
   imports :[
